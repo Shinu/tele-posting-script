ad85d715d56fe8c0311341c34646eb4202837038
Shinu <kester085@gmail.com>
2021-03-07 19:53:45 -0400

    New Add

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ api_hash = "f314c38f75d2f0e3b62289343c8740b5"
 #Enter your phone number.
 session_name = "18684880259"
 
-group_username = 1449511986
+group_username = 1293718914
 
-send_msg = "Testing" 
+send_msg = "Add me - Bain - 393-0560 - $50 x3" 
